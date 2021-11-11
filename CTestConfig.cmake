cff5c73aa270900ff863883802b599ebe6e2003d
Kim, Jungwon <kimj@ornl.gov>
2021-11-10 23:23:13 -0500

    added CTestConfig.cmake

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "mario")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=mario")
+set(CTEST_DROP_SITE_CDASH TRUE)
--- tests/01_repository_print/test01_repository_print.c
+++ tests/01_repository_print/test01_repository_print.c
@@ -8,12 +8,21 @@ int main(int argc, char** argv) {
   int group2 = argc > 3 ? atoi(argv[3]) : -1;
   int group3 = argc > 4 ? atoi(argv[4]) : -1;
 
+#if 1
   int match[3] = { mario_step, mario_level, mario_rank };
 
   mario_repository repo;
   mario_repository_create(path, &repo);
   mario_repository_build(repo, "plt([0-9]*)/Level_([0-9]*)/Cell_D_([0-9]*)", 3, match);
   mario_repository_print3D(repo, group1, group2, group3, stdout);
+#else
+  int match[2] = { mario_rank, mario_step };
+
+  mario_repository repo;
+  mario_repository_create(path, &repo);
+  mario_repository_build(repo, "hdf5_([0-9]*)_([0-9]*).h5", 2, match);
+  mario_repository_print2D(repo, group1, group2, stdout);
+#endif
 
   return 0;
 }
